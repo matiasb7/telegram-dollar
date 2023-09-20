4be8a73a02b5ec98ddcc18e70a1fb5394a407c3c
matiasb7 <matias.berger@rockstarcoders.com>
2023-09-20 15:28:41 -0300

    Allow multiple ids

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ load_dotenv()
 COINS_API = 'https://api.bluelytics.com.ar/v2/latest'
 TELEGRAM_API_TOKEN = os.environ.get('TELEGRAM_API_TOKEN')
 BOT_USERNAME = '@dolar_daily_bot'
-CHAT_ID = os.environ.get('CHAT_ID')
+chat_ids = os.environ.get('CHAT_ID').split(',')
 
 
 def get_coins():
@@ -23,11 +23,12 @@ def get_coins():
     return dollar_blue, euro_blue
 
 
-async def send_message(api_token, chat_id, message):
+async def send_message(api_token, chat_ids, message):
     bot = Bot(token=api_token)
-    await bot.send_message(chat_id=chat_id, text=message)
+    for chat in chat_ids:
+        await bot.send_message(chat_id=chat, text=message)
 
 
 dollar, euro = get_coins()
 message = f"Dollar: {dollar}, Euro: {euro}"
-asyncio.run(send_message(TELEGRAM_API_TOKEN, CHAT_ID, message))
+asyncio.run(send_message(TELEGRAM_API_TOKEN, chat_ids, message))
